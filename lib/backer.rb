a85642c62a4fb7ee19984b7ce454463a1a06c342
Stacey Astoveza <staceyastoveza@transfix.io>
2017-07-20 15:18:25 -0400

    Kick starter with Matt C

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/project'
+
+class Backer
+
+attr_accessor :name, :backed_projects
+
+
+ def initialize(name)
+ 	@name = name
+	@backed_projects = []
+
+ end
+
+ def back_project(project)
+ 	self.backed_projects << project
+ 	project.backers << self
+ end
+end
+
